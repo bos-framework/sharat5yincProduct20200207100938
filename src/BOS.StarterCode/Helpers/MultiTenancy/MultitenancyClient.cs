5c077aad83dccfb5ad7c281e4596cd6ec05ac833
BOSFramework <demo@bosframework.com>
2020-02-07 10:10:38 +0000

    Initial Commit

---
+++ src/BOS.StarterCode/Helpers/MultiTenancy/MultitenancyClient.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using BOS.StarterCode.Helpers.MultiTenancy;
+using System;
+
+namespace BOS.StarterCode.Helpers.HttpClientFactories
+{
+    public partial class ApiClient
+    {
+        public List<WhiteLabel> GetApplicationConfiguration(string baseurl)
+        {
+            List<WhiteLabel> response = new List<WhiteLabel>();
+            if (!string.IsNullOrEmpty(baseurl))
+            {
+                Uri appUri = new Uri(baseurl);
+                string appPath = string.Format("{0}://{1}", appUri.Scheme, appUri.Host);
+                string queryString = "?$filter=URL eq '" + appPath + "'&api-version=1.0";
+                var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                    "WhiteLabels"), queryString);
+                try
+                {
+                    var jsonResult =  GetSync<JObject>(requestUrl);
+                    if (jsonResult["value"] != null && jsonResult["value"].Count() > 0)
+                    {
+                        response = JsonConvert.DeserializeObject<List<WhiteLabel>>(jsonResult["value"].ToString());
+                    }
+                }
+                catch (Exception) { }
+            }
+            return response;
+        }
+
+    }
+}
